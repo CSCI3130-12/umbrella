934b8ae390473a996103c4782b679a38d641d058
jsedge <KingGuppie@gmail.com>
2018-03-09 12:57:48 -0400

    add fragment to view enrolled courses BB

--- app/src/main/java/com/umbrella/umbrella/MainActivity.java
+++ app/src/main/java/com/umbrella/umbrella/MainActivity.java
@@ -1,8 +1,15 @@
 package com.umbrella.umbrella;
 
 import android.content.Intent;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent testingIntent = new Intent(this, RegistrationActivity.class);
-        startActivity(testingIntent);
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.add(R.id.fragment,new MyCourseFragment(), "MyCourses");
+        transaction.addToBackStack(null);
+        transaction.commit();
+        //Intent testingIntent = new Intent(this, RegistrationActivity.class);
+        //startActivity(testingIntent);
     }
 }
