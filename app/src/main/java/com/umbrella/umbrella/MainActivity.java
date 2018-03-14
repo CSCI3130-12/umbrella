30a870933c0f8de910ed138809b0634dc66ec3e1
jsedge <KingGuppie@gmail.com>
2018-03-14 11:58:27 -0300

    seperate fragment into seperate method BB

--- app/src/main/java/com/umbrella/umbrella/MainActivity.java
+++ app/src/main/java/com/umbrella/umbrella/MainActivity.java
@@ -1,29 +1,29 @@
 package com.umbrella.umbrella;
 
 import android.content.Intent;
-import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manager = getFragmentManager();
 
-        FragmentManager manager = getFragmentManager();
+        Intent testingIntent = new Intent(this, RegistrationActivity.class);
+        startActivity(testingIntent);
+    }
+
+    public void showMyCoursesFragment(){
         FragmentTransaction transaction = manager.beginTransaction();
         transaction.add(R.id.fragment,new MyCourseFragment(), "MyCourses");
         transaction.addToBackStack(null);
         transaction.commit();
-        //Intent testingIntent = new Intent(this, RegistrationActivity.class);
-        //startActivity(testingIntent);
     }
 }
